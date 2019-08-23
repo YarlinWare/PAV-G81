783cbedb170d4f7f195e66a7a43b4a4ca72fcd11
Cyara <cmyarap@correo.udistrital.edu.co>
2019-08-22 21:16:22 -0500

    Se modifica el cague de datos
    Se optiene información a partir del Local Storage
    Se optiene información a partir de la URL
    Se modifican campos numéricos a partir de JavaScript

--- Tablas/js/Capturar_LocalStorage.js
+++ Tablas/js/Capturar_LocalStorage.js
@@ -1,50 +1,50 @@
 
 /*cargando del LocalStorage*/		
-var Nombre= document.getElementById('getnombre');
-Nombre.innerHTML=localStorage.getItem('nombre',nombre);
+var Nombre= document.getElementById('getnombreS');
+Nombre.innerHTML=localStorage.getItem('nombre');
+console.log(localStorage.getItem('nombre'));
+var Apellido= document.getElementById('getapellidoS');
+Apellido.innerHTML=localStorage.getItem('apellido');
 
-var Apellido= document.getElementById('getapellido');
-Apellido.innerHTML=localStorage.getItem('apellido',apellido);
-
-var Edad=document.getElementById('getedad');
-Edad.innerHTML=localStorage.getItem('edad',edad);
-
-/*   ---------------------   */
-var Nombre2= document.getElementById('getnombre2');
-Nombre2.innerHTML=localStorage.getItem('nombre2',nombre2);
-
-var Apellido2= document.getElementById('getapellido2');
-Apellido2.innerHTML=localStorage.getItem('apellido2',apellido2);
-
-var Edad2=document.getElementById('getedad2');
-Edad2.innerHTML=localStorage.getItem('edad2',edad2);
+var Edad=document.getElementById('getedadS');
+Edad.innerHTML=localStorage.getItem('edad');
 
 /*   ---------------------   */
-var Nombre3= document.getElementById('getnombre3');
-Nombre3.innerHTML=localStorage.getItem('nombre3',nombre3);
+var Nombre2= document.getElementById('getnombre2S');
+Nombre2.innerHTML=localStorage.getItem('nombre2');
 
-var Apellido3= document.getElementById('getapellido3');
-Apellido3.innerHTML=localStorage.getItem('apellido3',apellido3);
+var Apellido2= document.getElementById('getapellido2S');
+Apellido2.innerHTML=localStorage.getItem('apellido2');
 
-var Edad3=document.getElementById('getedad3');
-Edad3.innerHTML=localStorage.getItem('edad3',edad3);
-
-/*   ---------------------   */
-var Nombre4= document.getElementById('getnombre4');
-Nombre4.innerHTML=localStorage.getItem('nombre4',nombre4);
-
-var Apellido4= document.getElementById('getapellido4');
-Apellido4.innerHTML=localStorage.getItem('apellido4',apellido4);
-
-var Edad4=document.getElementById('getedad4');
-Edad4.innerHTML=localStorage.getItem('edad4',edad4);
+var Edad2=document.getElementById('getedad2S');
+Edad2.innerHTML=localStorage.getItem('edad2');
 
 /*   ---------------------   */
-var Nombre5= document.getElementById('getnombre5');
-Nombre5.innerHTML=localStorage.getItem('nombre5',nombre5);
+var Nombre3= document.getElementById('getnombre3S');
+Nombre3.innerHTML=localStorage.getItem('nombre3');
 
-var Apellido5= document.getElementById('getapellido5');
-Apellido5.innerHTML=localStorage.getItem('apellido5',apellido5);
+var Apellido3= document.getElementById('getapellido3S');
+Apellido3.innerHTML=localStorage.getItem('apellido3');
 
-var Edad5=document.getElementById('getedad5');
-Edad5.innerHTML=localStorage.getItem('edad5',edad5);
+var Edad3=document.getElementById('getedad3S');
+Edad3.innerHTML=localStorage.getItem('edad3');
+
+/*   ---------------------   */
+var Nombre4= document.getElementById('getnombre4S');
+Nombre4.innerHTML=localStorage.getItem('nombre4');
+
+var Apellido4= document.getElementById('getapellido4S');
+Apellido4.innerHTML=localStorage.getItem('apellido4');
+
+var Edad4=document.getElementById('getedad4S');
+Edad4.innerHTML=localStorage.getItem('edad4');
+
+/*   ---------------------   */
+var Nombre5= document.getElementById('getnombre5S');
+Nombre5.innerHTML=localStorage.getItem('nombre5');
+
+var Apellido5= document.getElementById('getapellido5S');
+Apellido5.innerHTML=localStorage.getItem('apellido5');
+
+var Edad5=document.getElementById('getedad5S');
+Edad5.innerHTML=localStorage.getItem('edad5');
--- Tablas/js/guardar_LocalStorage.js
+++ Tablas/js/guardar_LocalStorage.js
@@ -1,5 +1,17 @@
 var mensaje = document.getElementById('mensaje');
 var contmsg = document.getElementById('cont-msg');
+var boton= document.getElementById("btnAceptar");
+
+/*Ocultando el botón*/
+//boton.style.display="none";
+
+//Tipo input
+document.getElementById('edad').type="number";
+document.getElementById('edad2').type="number";
+document.getElementById('edad3').type="number";
+document.getElementById('edad4').type="number";
+document.getElementById('edad5').type="number";
+
 function validar(input){
  	mensaje.innerHTML = ""
   	contmsg.style.background='none';
@@ -9,14 +21,15 @@ function validar(input){
 		input.style.borderColor="#33CB0F";
     	mensaje.innerHTML = "Dato ingresado válido";
     	contmsg.style.background='#a0deb6';
-      	mensaje.style.color = "#000";
+      	mensaje.style.color = "#000"; 	
 		return true;
 	}else{
 		input.style.borderColor="#ff0000";
     	mensaje.innerHTML = "Dato ingresado inválido";      
-    	contmsg.style.background='#ff7588';
+    	contmsg.style.background='#ff7588';    	
 		return false;
 	}	
+
 }
 
 
@@ -24,18 +37,19 @@ function validarFormulario(formulario)
 {	
 	let result=true;
 	var input=formulario.querySelectorAll("input[type=text]");
-	var input_number=formulario.querySelectorAll("input[type=number]");
+	//var input_number=formulario.querySelectorAll("input[type=number]");
 	
 	for (i of input) {
 		if (validar(i)==false) {
 			result=false;
 		}
 	}
-	for (i of input_number) {
+	/*for (i of input_number) {
 		if (validar(i)==false) {
 			result=false;
 		}				
-	}			
+	}	*/	
+
 	return result;			
 }
 
@@ -44,48 +58,52 @@ function validarFormulario(formulario)
 //Get -> Obteniendo
 Form1.addEventListener('submit', function (event) {
 
-	// Don't submit the form
-	event.preventDefault();
+	//event.preventDefault();	/* Declarando variables*/
+	//Usuario1
 	var nombre = document.getElementById('nombre').value ;
 	var apellido = document.getElementById('apellido').value;
 	var edad = document.getElementById('edad').value ;
-	localStorage.setItem('nombre',nombre);
-	localStorage.setItem('apellido',apellido);
-	localStorage.setItem('edad',edad);
-
+	//Usuario2
 	var nombre2 = document.getElementById('nombre2').value;
 	var apellido2 = document.getElementById('apellido2').value ;
 	var edad2 = document.getElementById('edad2').value ;
-	localStorage.setItem('nombre2',nombre2);
-	localStorage.setItem('apellido2',apellido2);
-	localStorage.setItem('edad2',edad2);
-
+	//Usuario3
 	var nombre3 = document.getElementById('nombre3').value ;
 	var apellido3 = document.getElementById('apellido3').value ;
 	var edad3= document.getElementById('edad3').value;
-	localStorage.setItem('nombre3',nombre3);
-	localStorage.setItem('apellido3',apellido3);
-	localStorage.setItem('edad3',edad3);
-
+	//Usuario4
 	var nombre4 = document.getElementById('nombre4').value;
 	var apellido4 = document.getElementById('apellido4').value;
 	var edad4 = document.getElementById('edad4').value;
+	//Usuario5
+	var nombre5 = document.getElementById('nombre5').value;
+	var apellido5 = document.getElementById('apellido5').value;
+	var edad5 = document.getElementById('edad5').value;
+
+		
+	localStorage.setItem('nombre',nombre);
+	localStorage.setItem('apellido',apellido);
+	localStorage.setItem('edad',edad);
+		
+	localStorage.setItem('nombre2',nombre2);
+	localStorage.setItem('apellido2',apellido2);
+	localStorage.setItem('edad2',edad2);
+	
+	localStorage.setItem('nombre3',nombre3);
+	localStorage.setItem('apellido3',apellido3);
+	localStorage.setItem('edad3',edad3);
+	
 	localStorage.setItem('nombre4',nombre4);
 	localStorage.setItem('apellido4',apellido4);
 	localStorage.setItem('edad4',edad4);
+	
+	localStorage.setItem('nombre5',nombre5);
+	localStorage.setItem('apellido5',apellido54);
+	localStorage.setItem('edad5',edad5);
 
 }, false);
 
-			
-/*function comprobar(){
-	var input=formulario.querySelectorAll("input[type=text]");
-	var input_number=formulario.querySelectorAll("input[type=number]");
-	for (i of input){
-		for (i of input_number){
-			if(input==true && input_number==true){
-				boton.disabled= false ;				
-			}
-		}
-	}
-	
+/*En desarrollo: Validación de todos los campos*/
+/*if (document.getElementById('edad5').value==true) {	
+    boton.style.display="initial";     
 }*/
